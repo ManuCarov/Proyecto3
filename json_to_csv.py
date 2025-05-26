4a3c580b4f207c5f8ee7bec5d097a270f7d23935
Manuela Caro Villada <104787569+ManuCarov@users.noreply.github.com>
2025-05-25 21:17:29 -0500

    Datos
    Los datos del clima en medellin en el año 2022

---
+++ json_to_csv.py
@@ -0,0 +1,15 @@
+import json
+import csv
+
+with open("clima_medellin_2022.json") as f:
+    data = json.load(f)
+
+with open("clima_medellin_2022.csv", "w", newline="") as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(["date", "temperature_max", "precipitation"])
+    for i in range(len(data["daily"]["time"])):
+        writer.writerow([
+            data["daily"]["time"][i],
+            data["daily"]["temperature_2m_max"][i],
+            data["daily"]["precipitation_sum"][i]
+        ])
